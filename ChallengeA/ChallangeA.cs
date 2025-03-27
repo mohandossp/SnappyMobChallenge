9c8bd6183109570701c25e1f5c9b881095576f63
mohandossp <mohandoss.palani@outlook.com>
2025-03-28 00:14:44 +0800

    Add project files.

---
+++ ChallengeA/ChallangeA.cs
@@ -0,0 +1,171 @@
+﻿using System.Text;
+
+namespace ChallengeA
+{
+    public class ChallangeA
+    {
+        public interface IPrintableObjects
+        {
+            string GetValue();
+        }
+
+        public class AlphabeticalStrings : IPrintableObjects
+        {
+            private static readonly Random _instanceRandom = new Random();
+            private string _result;
+            public AlphabeticalStrings()
+            {
+                _result = string.Empty;
+            }
+            public string GetValue()
+            {
+                var ContentLength = _instanceRandom.Next(10, 20);
+                StringBuilder stringContent = new StringBuilder();
+                for (int i = 0; i < ContentLength; i++)
+                {
+                    stringContent.Append((char)_instanceRandom.Next('a', 'z' + 1));
+                }
+                _result = stringContent.ToString();
+                return _result;
+            }
+        }
+
+        public class RealNumbers : IPrintableObjects
+        {
+            private static readonly Random _instanceRandom = new Random();
+            public RealNumbers()
+            {
+
+            }
+            public string GetValue()
+            {
+                return _instanceRandom.NextDouble().ToString("F2");
+            }
+        }
+        public class Integers : IPrintableObjects
+        {
+            private static readonly Random _instanceRandom = new Random();
+            public Integers()
+            {
+
+            }
+            public string GetValue()
+            {
+                return _instanceRandom.Next(1, 1000).ToString();
+            }
+        }
+        public class AlphaNumerics : IPrintableObjects
+        {
+            private static readonly Random _instanceRandom = new Random();
+            private int MaxEmptySpaceCount = 10;
+
+            public AlphaNumerics()
+            {
+                MaxEmptySpaceCount = 10;
+            }
+
+            public AlphaNumerics(int _MaxEmptySpaceCount)
+            {
+                MaxEmptySpaceCount = _MaxEmptySpaceCount;
+            }
+            public string GetValue()
+            {
+                var length = _instanceRandom.Next(10, 20);
+                StringBuilder sb = new StringBuilder();
+                for (var i = 0; i < length; i++)
+                {
+                    if (_instanceRandom.Next(0, 3) == 0)
+                    {
+                        sb.Append((char)_instanceRandom.Next('a', 'z' + 1));
+                    }
+                    else
+                    {
+                        sb.Append((char)_instanceRandom.Next('0', '9' + 1));
+                    }
+                }
+
+                var EmptySpaceCountFirst = _instanceRandom.Next(0, MaxEmptySpaceCount + 1);
+                var EmptySpaceCountLast = MaxEmptySpaceCount - EmptySpaceCountFirst;
+
+                var result = String.Concat(new string(' ', EmptySpaceCountFirst),
+                                            sb.ToString(),
+                                            new string(' ', EmptySpaceCountLast));
+
+                return result;
+
+            }
+        }
+
+        public class PrintableObjects
+        {
+            private readonly IEnumerable<IPrintableObjects> ListprintableObjects;
+
+            public PrintableObjects()
+            {
+                ListprintableObjects = new IPrintableObjects[]
+                {
+                new AlphabeticalStrings(),
+                new RealNumbers(),
+                new Integers(),
+                new AlphaNumerics()
+                };
+            }
+            public PrintableObjects(int MaxSpaceCount)
+            {
+                ListprintableObjects = new IPrintableObjects[]
+                {
+                new AlphabeticalStrings(),
+                new RealNumbers(),
+                new Integers(),
+                new AlphaNumerics(MaxSpaceCount)
+                };
+            }
+            public void Print(string FilePath, long MaxSize)
+            {
+                var FileSize = 0;
+                var MaxFileSize = BytesToMB(MaxSize);
+                var SbFileConent = new StringBuilder();
+                var _InstanceRandom = new Random();
+
+
+                if (File.Exists(FilePath))
+                {
+                    File.Delete(FilePath);
+                }
+
+                if (!File.Exists(FilePath))
+                {
+                    using (File.Create(FilePath))
+                    {
+
+                    }
+                }
+
+                using (var sw = new StreamWriter(FilePath))
+                {
+                    while (FileSize < MaxFileSize)
+                    {
+                        var Index = _InstanceRandom.Next(0, ListprintableObjects.Count());
+                        var PrintObject = ListprintableObjects.ElementAt(Index);
+
+                        var PrintObjectResult = PrintObject.GetValue();
+                        SbFileConent.Append(PrintObjectResult + ",");
+
+                        FileSize += Encoding.UTF8.GetByteCount(PrintObjectResult + ",");
+                        if (FileSize >= MaxFileSize)
+                        {
+                            break;
+                        }
+                    }
+
+                    sw.Write(SbFileConent.ToString().TrimEnd(','));
+                }
+            }
+
+            private long BytesToMB(long bytes)
+            {
+                return bytes * 1024 * 1024;
+            }
+        }
+    }
+}
---
+++ ChallengeB/ChallengeB.cs
@@ -0,0 +1,154 @@
+﻿using ChallengeA;
+using System.ComponentModel;
+
+namespace ChallengeB
+{
+    internal class ChallengeB
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter Maximum Space Count to append before and after in AlphaNumberic.");
+            //int MaxEmptySpacesNumber = int.Parse(Console.ReadLine());
+            //if (MaxEmptySpacesNumber >= 0 && MaxEmptySpacesNumber <= 100)
+            //    Console.WriteLine("Entered: " + MaxEmptySpacesNumber);
+            //else
+            //    Console.WriteLine("Entered Invalid Number. Enter number in between 0 to 100");
+
+            Console.WriteLine("Challange A - Starts");
+            int MaxEmptySpacesNumber = 10;
+            int MaxFileSize = 10;
+            string FileName = "/app/data/";
+            var InputFileName = FileName + "input/PrintObjectsResultA_Input.txt";
+            var PrintObjectA = new ChallangeA.PrintableObjects(MaxEmptySpacesNumber);
+            PrintObjectA.Print(InputFileName, MaxFileSize);
+            Console.WriteLine("Challenge A File Saved successfully");
+            Console.WriteLine("Challange A - End");
+
+
+            Console.WriteLine("Challange B - Starts");
+            var PrintObjectB = new ChallangeB.PrintableObjects();
+            PrintObjectB.Print(InputFileName);
+
+            var OutPutFileName = FileName + "output/PrintObjectsResultB_Output.txt";
+            PrintObjectB.Write(InputFileName, OutPutFileName);
+            Console.WriteLine("Challenge B File Saved successfully");
+            Console.WriteLine("Challange B - End");
+        }
+    }
+
+    internal class ChallangeB
+    {
+        private enum PrintTypes
+        {
+            [Description("Alphabetical Strings")]
+            AlphaCharacters,
+            [Description("Integers")]
+            Integers,
+            [Description("Real Numbers")]
+            RealNumbers,
+            [Description("AlphaNumerics")]
+            Alphanumerics
+        }
+        public class PrintableObjects
+        {
+            public void Print(string FilePath)
+            {
+                var FileContent = File.ReadAllText(FilePath);
+                var PrintContent = FileContent.Split(',');
+
+                foreach (var str in PrintContent)
+                {
+                    string value = str.Trim();
+                    if (IsAlphaCharacters(value))
+                    {
+                        Console.WriteLine($"Print Object: {value}, Type: {PrintTypes.AlphaCharacters.ToString()}");
+                        continue;
+                    }
+                    else if (IsIntegers(value))
+                    {
+                        Console.WriteLine($"Print Object: {value}, Type: {PrintTypes.Integers.ToString()}");
+                        continue;
+                    }
+                    else if (IsRealNumbers(value))
+                    {
+                        Console.WriteLine($"Print Object: {value}, Type: {PrintTypes.RealNumbers.ToString()}");
+                        continue;
+                    }
+                    else if (IsAlphaNumerics(value))
+                    {
+                        Console.WriteLine($"Print Object: {value}, Type: {PrintTypes.Alphanumerics.ToString()}");
+                        continue;
+                    }
+                }
+            }
+            public void Write(string FilePath, string OutputFilePath)
+            {
+                var FileContent = File.ReadAllText(FilePath);
+                var PrintContent = FileContent.Split(',');
+
+                if (File.Exists(OutputFilePath))
+                {
+                    File.Delete(OutputFilePath);
+                }
+
+                if (!File.Exists(OutputFilePath))
+                {
+                    using (File.Create(OutputFilePath))
+                    {
+
+                    }
+                }
+
+                using (StreamWriter Write = new StreamWriter(OutputFilePath, true))
+                {
+                    foreach (var str in PrintContent)
+                    {
+                        string value = str.Trim();
+                        if (IsAlphaCharacters(value))
+                        {
+                            Write.WriteLine($"Print Object: {value}, Type: {PrintTypes.AlphaCharacters.ToString()}");
+                            continue;
+                        }
+                        else if (IsIntegers(value))
+                        {
+                            Write.WriteLine($"Print Object: {value}, Type: {PrintTypes.Integers.ToString()}");
+                            continue;
+                        }
+                        else if (IsRealNumbers(value))
+                        {
+                            Write.WriteLine($"Print Object: {value}, Type: {PrintTypes.RealNumbers.ToString()}");
+                            continue;
+                        }
+                        else if (IsAlphaNumerics(value))
+                        {
+                            Write.WriteLine($"Print Object: {value}, Type: {PrintTypes.Alphanumerics.ToString()}");
+                            continue;
+                        }
+                    }
+                }
+
+            }
+            private bool IsAlphaCharacters(string str)
+            {
+                return !string.IsNullOrEmpty(str) && str.All(char.IsLetter);
+            }
+            private bool IsIntegers(string str)
+            {
+                return !string.IsNullOrEmpty(str) && str.All(char.IsDigit);
+            }
+            private bool IsRealNumbers(string str)
+            {
+                bool result = false;
+                if (Double.TryParse(str, out double res))
+                {
+                    result = str.Contains(".");
+                }
+                return result;
+            }
+            private bool IsAlphaNumerics(string str)
+            {
+                return !string.IsNullOrEmpty(str) && str.All(char.IsLetterOrDigit) && str.Any(char.IsLetter) && str.Any(char.IsDigit);
+            }
+        }
+    }
+}
